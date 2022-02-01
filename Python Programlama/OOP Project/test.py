cf53926d5fc3a0c563d5382614b16e602ae184be
ygtozen <64047525+ygtozen@users.noreply.github.com>
2022-02-01 15:27:34 +0300

    OOP Project
    Python Proggramming with OOP Project

---
+++ Python Programlama/OOP Project/test.py
@@ -0,0 +1,34 @@
+
+import HesapModulu
+
+HesapModulu.yeni_maas(1000)
+HesapModulu.yeni_maas(2000)
+
+import HesapModulu as hm
+
+hm.yeni_maas(3000)
+
+from HesapModulu import yeni_maas
+yeni_maas(4000)
+ 
+
+import HesapModulu as hm
+hm.maaslar
+
+################################################♥
+
+import CalismaHesapModulu 
+
+CalismaHesapModulu.yeni_maas(5000)
+CalismaHesapModulu.yeni_maas(1000)
+
+import CalismaHesapModulu as chm
+chm.yeni_maas(2000)
+
+from CalismaHesapModulu import yeni_maas
+yeni_maas(3000)
+
+
+import CalismaHesapModulu as chm
+chm.maaslar
+
